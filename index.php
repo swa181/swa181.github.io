9029e37a4972aaf945c14374372ba71d034f1844
swa181 <swa181@sfu.ca>
2018-05-03 11:42:54 -0700

    changed to php successfully

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>sandy wu.</title>
+  <meta charset="utf-8"/>
+  <meta name="author" content="Sandy Wu">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" crossorigin="anonymous">
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Francois+One" rel="stylesheet">
+  <style>
+  @import url('https://fonts.googleapis.com/css?family=Abel|Ropa+Sans|Scada:700');
+  </style>
+  <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+  <header class="container">
+    <?php include 'header.php';?>
+  </header>
+
+  <section style="background-color:#ffcc00" class="jumbotron">
+    <div class="container">
+      <h2>PORTFOLIO: SANDY WU</h2>
+      <h3 style="text-align:left">
+          I am currently a student at Simon Fraser University.</br>
+          I like creating things.</br>
+          Based in Vancouver, Canada.
+      </h3>
+    </div>
+  </section>
+
+<!-- van dusen -->
+  <section class="container">
+      <img class="img-responsive menubut" src="https://4.bp.blogspot.com/-fFDvktB0dXw/WuN79fdGywI/AAAAAAAADSE/1E5_79g2aDUycOL2G2z4r7M7BgFaKxnrACLcBGAs/s1600/01fba5a7861b14f0c84ec109f21951f9decbb9d7f9.jpg">
+  </section>
+  <section class="container">
+    <div class="row">
+      <figure class="col-sm-6">
+        <img src="https://4.bp.blogspot.com/-KHskPqd3vKU/WuPpQTk7OXI/AAAAAAAADUE/bEoujqbZan4x1k46nq2WLzROmMJf_iwVQCLcBGAs/s1600/dusen-qe.png">
+      </figure>
+      <figure class="col-sm-6">
+        <h6>APRIL 26, 2018</h6>
+        <h3>VAN DUSEN GARDEN</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu erat ac lacus hendrerit ornare at eu nulla. Etiam tincidunt, mauris at auctor sagittis, ligula risus mollis ligula, eget vehicula tortor ligula tristique lorem. Donec dapibus, orci vel iaculis congue, dui turpis faucibus mauris, id faucibus nisi leo vitae urna. Quisque quam lectus, consequat a ultrices at, commodo eu libero. Suspendisse vel libero at lacus elementum consequat quis sit amet felis. Nullam et aliquam sapien. Proin interdum fringilla orci vel venenatis. Nulla eu fermentum dolor.</p>
+      </figure>
+    </div>
+    <div class="row">
+      <figure class="col-sm-6">
+        <h3>お花です</h3>
+        <p>私の元気木曜日。</p>
+        <img src="https://2.bp.blogspot.com/-W8NpcSU79GA/WuSZGrZ98hI/AAAAAAAADU0/J3p9CDeVSLEh5G1AzDU2osAv5MOsgwiQQCLcBGAs/s1600/blossoms.png">
+      </figure>
+      <figure class="col-sm-6">
+        <img src="https://4.bp.blogspot.com/-fbRECtUPxSk/WuPuAD-7PzI/AAAAAAAADUU/PtxaPELEOrsf0PgguFCrZmkqQjpiY7KEgCLcBGAs/s1600/blossom.png">
+      </figure>
+    </div>
+  </section>
+
+
+<section class="container"><hr/></section>
+<section class="container">
+  <div class="row">
+    <figure class="col-sm-4">
+      <img  src="https://lh3.googleusercontent.com/hQpfC4WfSXn1UlOC1gnp7PUFtxR8oX_o1v6WqsOPfDDo33501OnnCl8SHrXaQaB5FdOtssxnzSv6XjBBa0o5cVFhLi5zzBuwrrtexThI2LoIL5zRicTWWNcBcxtj8ynGy6u-zSbaoteM-H1CsJCNdOx6qPHgRjEy_saNpeLHQlqed3LSZHheKHMUPP_06Cuorsyym0kniiC-9jyT-9bBc4PZbMty8cABES8uGA4n7PW86XeHnrdbNfJdR-MVnIaSjzZUT-UoILyuVUxoahV9gaMH6bV7ZemH7ScdMSljT6n4qozC3BrJyJGSJq3j5mH_bFyJkNTUN2KIbPGpKi8MXjkcTQDz67AmLoOc4Ack4YzqQdq7zSKKCxg4j3mxWkMNEvGfjTLEBFuOgvbEhlqe_pLD8nrfbou0q3z-Tk0wQ6ACEBnaXEZK2V3Jcb_bojXPdD4TRRQQkoTQRRpUpfqLxr0WtHZRtOmCWJLgvmPl0YwT1Q2UwdT3hqggHIcTuwwTUk1RCYVt0tneFKc5RAx0Pa3MtHBfwTHma_2mbPqkdg0GLZSRCZbUDvTDhV0N3Pi5pouLtUb6cuJTqrDilGR_vRFm2Z52LtEZPyTk9Ho=w653-h662-no">
+      <div style="background-image:url(https://lh3.googleusercontent.com/jIs48AdPiWLWFXe-mcBt0Av2jLNUfYTBeECgfPidCjhWP_biwi7L2GQZUX9m86Xh5Ll27TwT9qzu7838zVZDA4Uijh5QQg7UTPEPvoqus8ZkONP5_Hmjox99TFHfGInDIjyh6w5ApPj7O5TmiBda4CHe99jdKo4T8oM_-OCbojgQUdoYBbSgSIZmuoAk3y5Jd2CQwzG3qrEp7g8PcNr5iVcmPZ0ZC_ZUlYSR7TgkQO260T5mT5LRc2r86JalXBjEDndalk6G4LxOhOKbFcoAGaVAXxbXhPmWL3WODWwDnKiGjWn1SyehE2-Mi-7FRlaQ1Pvd9Xx23BdIeavPyIT4Cxx_XsXC2VpSxIwyvcRUbU3cPJfGdQS3c7NUGdCKnNyFtwvYiWuCDQBzZ2ieNsp9rXc4CgklWb-7W2rTZXVBZFDWv2m7tYLzBZVH5sTVuKfcx-26oADeOfFeUYhoycwazSOF4vDtTLTVwxlMoh2mC4yDvJqXPNr77QRUdhweC6TSodWW_0h1bwoPAy9D4wc3hevtM-RGB9zHtI_rXoQvvTCmocErl_NVV9PE4nRrc3unQ_fI1nkTAbT9ey-ZOTgVNLjCWD3uDmZ5sHVQAfk=w712-h949-no)" class="centered overlay"></div>
+      <h3>Chrysanthemum</h3>
+      <p>I really like Chrysanthemum tea at Chinese restaurants, but my parents always get Jasmine tea. This flower doesn't look drinkable though. Commonly known as the White Button Pom.</p>
+    </figure>
+    <figure class="col-sm-4">
+      <img src="https://lh3.googleusercontent.com/9XiMzYDO9U7xWONOQeHRqk81aoEhIN5tK9AoLpefIWZCCWXcCS3ni2Iq4-smfWHYV8594GFXcwAwcVEXGwnrGreSP5JZqM28lFObz3CRMyha2E_2Y8jCy6m2YeUzwNEgyaDTlSos4T7aaAMS5fHoA459xcXVxYEp2mr9ljZLBN0_U27Bh_OJa0GN2YVa4GUs1mv2a6pIeSSlE2TAk_Qgqwp0cEY6be5fCwUDciGqpavwrP9WK9WeYNLrFiO2Ejf35RpK4te9EWooJiGBmxOHm3TM_-CsAZpIGeM9MyY4x8yDYU7P1PiZKBcCAcr2G0kb9DXw-ieq7y0Rt5n0ZfuNZV08w0p4AnakdHvqqKHUTAheQKowZ0rVz0fWT4WSOVhRPuZ6i3WBlGEQq02ZwSy8SWlQrg5RZvHomxYruJ7ejh9LZLm-4Or0yqkZzpqLElb1K_noiD3E-62hHHlv3UMjr-7kMkwb-gMXwbixmmCMws1kt7ilgHy8eQzsH2ARCmCHcljVvU7uZdj0U1O8rRnFzrN9XHTWQ_AyvTqWIpDrD_x4fZI01MSSBJvaDXPoBcOlhv0tm6WNcMaMc-3kJSFJIaZHtjeGrExepUXXiu0=w653-h662-no">
+      <div style="background-image:url(https://lh3.googleusercontent.com/yS0KkU0tTBelpnMaNcXUC-2d4uXlPspPbz-wOQ_YDSP2oGWueyADM3n826kgcaLF7BoHaAedy8e5YgQhTGU0_s4wHWyeFJ6Kp0M8UZCaGp1msaI_ynElQFKoQQA0jHLSJpq3FKCAeM-MorXpnx3hOBiLiT05QVClMSZZGvvdnDrRALVgrWjeQYD7tKNLPLkijXYwv1I9Q7J7QVwgekulJys55RP2tn4gpO_v4sqSr5YTiUxqdkz3dg2uaBnz5IOUrmNmYL_U1-uxikgq5XNz9ntF8PLSZdwOF8tfZqJy5gEJ5BwZRLSdrPEifFfCuJfGduj0wiIn3vx-I398RDQeUWeV5v9D0ao9pS-fWBte1hSazDMIxaZ4jVEG_4njbuesnb3qKjBwVSLsNZ6q0cANdDkVeNasYmg7VkyJjOXfDIyISOYThdVTK-9cMqPXd0TfQ74z74qgvciUNP2IbODadS6y47Oo5IcXxd58W1q8mRxTXxq0VfH2dRUUVBpSKQoqpo4BoRhhq9YrwlaMthj7J22atj_8gDMzNyhFgn-CrUliPD0lObGBwv4KEIXvgO7ky3PtkkolZ7EukhJi-n0h0tE7HHcXKonJLHrwjQo=w712-h949-no)" class="centered overlay"></div>
+      <h3>Rhododendron</h3>
+      <p>This is essentially the bigger version of the cherry blossom flower. It's really pretty in person. The streets on the West End around Van Dusen are coated with these.</p>
+    </figure>
+    <figure class="col-sm-4">
+      <img src="https://lh3.googleusercontent.com/O2IegOjf3hGV1Oul6kOish8-8KYxgYVoiabiQQKyaqCwhlxWZFI9PdRXfmPAaUOmnycwb00VLqh7SakfxLitYAO7JXmHhBdnzxcncwCJq1kkYmLCG9kIIrnt0V1sH2bL-H-_fAIlSrBDH_Hu52xdUJrQqI6euYRmU0iBaUFKEbNtritqeN54S2LHFev9W9gwTc0kkWbCNAhUC6LmgnrdVaDzrwGZy60wkwY-o214xM_Hd5-86x4R7adGr4acJyEDqeyQtyKcRVrxzePQKDb53JAn6U-3ToxIY3gLDdy5ck9WaTFz2vUi9Qu2__PK8JEgF-jYZ8XqfJf1WbvdtLDiUoTHr907PNqjDM_XGFzyVwkEdSKuGhuDSn-YC4l_URAolSlsTETaDkygLqR2HQ745FHKvohyelR34Vatdoaot7KMJhkL7GPaI-ynnkFuCoxFEVMRWdr2Ub55axVeWdxDpucihxlKELNjcW77En1QWTdkwESWeX0fQPITo42vUfwtf01oWiUKdlljeDnIl-C_k3UExKr1lvkXhjlHI6AuizE3lWPpQqpE0v75XGQXV2TjHPcn738W5tZirHLKYe1WpwF7VKZWvdRkNBhoHtM=w653-h662-no">
+      <div style="background-image: url(https://lh3.googleusercontent.com/4T6NoA7PneM6wWR3QogDBY9ERiK7LxQBqWYFTitkQ1gdxWV7wHogqGXtxMxlxb0exxsUGjP925LAsnAcDg07S37-zwicSAdjCnhYI9vX3_-Uvn7U7PRVCgrgpXbu0Z4BeontmY0tjstU95lrzysMy6h7CrWuXWF4AMyEt4ZFozTmdUID8sDjWuv8kKQsKTBJqocISWgSQknKvYxSLjEFd7djjBdgMaiVP73LLQ508kz9QuqjLPrvGQiBpzgZ54NlB7-ATJzLcpL4SYqZVp9pFEl5EkWrX2QVzmyZmZdN4VsWbP6pPlgwf-ZvvhyeqWzo7iVY9iiHc-pLlCBfUqN0JiGzjnEd-qiEKG9GmxoCpTCrHfh4Avs1vYCTpY3ZJRjfYXNMMQapxQSCqSEagrkKuOzIO_z1bkv_2S8lnKOv5WNPXKxMdBF4nC0RnXablAIFbkEir1Osmw7gfIJmxuHV7hZghsZf67rWvQoaus-tSvuZWXK-ffDLaNFrpzPBEeobJ8TD8kY55IO_bisO5jE0HyUtNIuAceINmqR6WRJDc9fHCiY2iMWj2NzXbQoNiwLEU85NEsklSxf50EZBrWwxfk2HaWWm2UemmXH_eyg=w712-h949-no) " class="centered overlay"></div>
+      <h3>Primrose</h3>
+      <p>Little violet flowers. The fact that the purple color is naturally juxtaposed with the yellow center is intriguing. It looks rare; I wish I knew its' proper scientific name.</p>
+    </figure>
+  </div>
+<section class="container"><hr/></section>
+
+</section>
+
+<section class="container">
+    <img class="img-responsive menubut" src="https://lh3.googleusercontent.com/coWi69nhh17xelhQeVhxX2BmJtDNVNB1wF1jL-zLrSDgX3wlgBDHLmDsDvNbZOMkT-1pF0zpqenWzNUyePkR1Nzjr0VythvZszcRC5USCRnZCLFFtMAqPM4jnkx6ED8QEYICuXYuhCvvgTfceFCRLqqdXlu7PnwpuX-i2a_v_UBJiielE2fsA8dhWecCwJZADvg1ShzyLfJmMox_kO2qwrzgXkwVGok0u2cduqVjw2NTQodUq21WoTswx7W2eukRgntqzZCKB51Fxu2InOJGQTp3ZFUG_UcYgWAdsRydb5nqruQxnpD1NtF6a5nARb5wHA9WPyKOKc_lYzboySs_NZMCBF_f59yBxA95Fs4y04ALyiz8GbTjE2PQWsyD3ivMl-dBnLAft9nQTcuSnhK-td3nLfcKqGvMzKXIfiAPPoZ7o187kNNbTPUvMXsy61EGhyzceBRpQA-tddcPyOSZGJoSoXq7LB6lFMecpQi93HgUtpYzrnImxxkZshZzQ4LdV65uyx3CFPtAvj0zticMxweRcLi8NyPV_KOk53Mx4q4ZfbY2MSPaMpLkckgoYmfZx_sqBvXsyktJJeIfqT2Quf9nSKSX4MLr53ZQpZY=w1921-h672-no">
+</section>
+
+
+<script src="functions.js"></script>
+
+</body>
+
+<footer>
+  <?php include 'footer.php';?>
+</footer>
+</html>
+
---
+++ website_about.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>sandy wu.</title>
+  <meta charset="utf-8"/>
+  <meta name="author" content="Sandy Wu">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" crossorigin="anonymous">
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Francois+One" rel="stylesheet">
+  <style> @import url('https://fonts.googleapis.com/css?family=Abel|Ropa+Sans|Scada:700');  </style>
+  <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+  <header class="container">
+    <div class="row">
+      <h1 class="col-sm-4">sandy wu.</h1>
+      <nav class="col-sm-8 text-right">
+        <a href="website.html">home</a>
+        <a href="website_about.html">about</a>
+      </nav>
+    </div>
+  </header>
+
+  <section style="background-color:tomato" class="jumbotron">
+    <div class="container">
+      <h2 style="color:yellow">
+        ABOUT ME
+      </h2>
+    </div>
+  </section>
+
+  <section class="container">
+    <h3 style="text-align:left">How I created this site</h4>
+    <p>
+      My main source of motivation was a free Codecademy course on Bootstrap.</br>
+      Every detail (except for the fonts) was either drawn by me or pieced together by me.</br>
+      There's a lot of filler content for the lols and giggles.</br>
+      W3 Schools guided me in adding extra details and Javascript.
+    </p>
+    <h3 style="text-align:left">What is this site about?</h4>
+    <p>
+      Before you message me about what this website is about, I hope you read this. </br>
+      I'm just learning HTML, CSS, and Bootstrap right now.</br>
+      I'm not making this for a special purpose other than to test features and get a feel for web development.
+    </p>
+    <h3 style="text-align:left">Languages and Skills</h4>
+    <p>
+      C/C++. Python. HTML. CSS. Bootstrap.
+    </p>
+    <h3 style="text-align:left">Volunteer Experiences</h4>
+    <p>
+      Vancouver Sun Run. Green Chair at BMO Marathon. Art Attack Program at Sir Matthew Begbie Elementary.
+      KidSafe at Grandview Elementary. Food Bank at Broadway Church. English Tutor at Love Your Neighbour.
+      After School Walk from Laura Secord at Broadway Church. Bright Nights at Stanley Park.
+    </p>
+    <h3 style="text-align:left">Work Experiences</h4>
+    <p>
+      Guest Services Representative at Pacific National Exhibition. Ticket Seller at PNE. Cashier at T&T Supermarket.
+      Administrative Assistant at Financial Literacy Counsel.
+    </p>
+  </section>
+</body>
+
+</html>
